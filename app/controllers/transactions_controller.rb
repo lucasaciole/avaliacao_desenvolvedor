a610503df447fb87bed407502183c45b60318bb1
Lucas Y. S. Aciole <lucas.aciole@gmail.com>
2018-05-16 21:44:38 -0300

    Create import action on TransactionsController

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -61,6 +61,10 @@ class TransactionsController < ApplicationController
     end
   end
 
+  # POST /transactions/import
+  def import
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_transaction
