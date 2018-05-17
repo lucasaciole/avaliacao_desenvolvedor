0aa5b2015b3aa060badf2029fdabe4d0a07d6dc2
Lucas Y. S. Aciole <lucas.aciole@gmail.com>
2018-05-16 23:36:28 -0300

    Fix transaction import route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :transactions
-  post '/transactions/import', to: 'transactions#save_import'
+  post '/transactions/import', to: 'transactions#import', as: 'import_transactions'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
