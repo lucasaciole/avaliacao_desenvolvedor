c67df2ba0cdaa7a888e8f05362edb1ae46c876cd
Lucas Y. S. Aciole <lucas.aciole@gmail.com>
2018-05-17 01:56:54 -0300

    Set Transactions index as app index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'transactions#index'
   resources :transactions
   post '/transactions/import', to: 'transactions#import', as: 'import_transactions'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
